61ca6979c2f572bf96274e60d9ac34ee5ba2e768
Thierry Beer$ <thbeer@student.ethz.ch>
2024-09-30 13:58:14 +0200

    added cool stuff

---
+++ Coolfunctions.m
@@ -0,0 +1,16 @@
+classdef Coolfunctions
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        loopsize
+        N
+    end
+    
+    methods
+        function obj = Coolfunctions(loopsize, N)
+            obj.loopsize = loopsize;
+            obj.N = N;
+        end
+    end
+end
