c35c1d6f18686ce24649af6d5276f74fb9cbe5cf
Thierry Beer$ <thbeer@student.ethz.ch>
2024-10-14 16:09:17 +0200

    week3 update

--- week 3/scipt_w3.m
+++ week 3/scipt_w3.m
@@ -1,21 +1,44 @@
 u = Util();
 
-N_rep = 4;
+N_rep = 1;
 q = 1e-1;
 
-s = Source(100, 0.6).bit_sequence;
+
+s = Source(10, 0.6).bit_sequence;
 s_enc = u.repencode(s, N_rep);
+
+figure(3);
+subplot(5, 1, 1);
+scatter(1:length(s_enc), s_enc);
+
 s_key = u.add_keys(s_enc);
+
 s_mod = modulator.modulate(s_key);
+figure(3);
+subplot(5, 1, 2);
+plot(1:length(s_mod), s_mod);
 
 %channel----
-s_modnoise = awgn(s_mod, 10);
-s_someerr = [zeros(1, 136), s_modnoise, zeros(1,28)];
+s_modnoise = awgn(s_mod, 0);
+s_someerr = [zeros(1, 345), s_modnoise, zeros(1,28)];
+figure(3);
+subplot(5, 1, 3);
+plot(1:length(s_someerr), s_someerr);
 %----------------
 
 s_symsync = Sync.symbolsync(s_someerr);
 s_demod = modulator.demodulate(s_modnoise);
+
+figure(3);
+subplot(5, 1, 4);
+scatter(1:length(s_demod), s_demod);
+
 s_frame = Sync.framesync(s_demod);
+
+figure(3);
+subplot(5, 1, 5);
+scatter(1:length(s_frame), s_frame);
+
 s2_dec = u.repdecode(s_frame, N_rep);
 ber = BER.calc_BER(s, s2_dec)
 d = Drain(s2_dec);
